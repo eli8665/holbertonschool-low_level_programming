f52b3b78761e00381c8978ad2b737dd5737ba170
eli8665 <elham2707@gmail.com>
2024-06-18 08:48:10 +0000

    project 8 task 5-1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*main- print exacly with proper grammar
+*Return:0
+*/
+int main(void)
+{
+printf("withe proper grammar, but the outcomme is a piece of art,\n");
+return (0);
+}
