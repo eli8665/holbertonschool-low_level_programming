0d9bd6cc707c12cd4e34c57af43e701ed28dbd3a
eli8665 <elham2707@gmail.com>
2024-06-19 12:00:49 +0000

    project 9 task 0-6

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 int main(void)
-	/**
- * main -  n is positive or negative.
- * Cette fonction affiche un message a l'ecran en utilisant puts.
- * Return: Toujours 0 (Succes)
- */
 {
-	int n;
-		srand(time(NULL));
-		n = rand();
-		printf("the number %d is", n);
-	if (n > 0)
-	{
-	printf("is positive,\n");
-	}
-	else if (n == 0)
-	{
-	printf("is zero,\n");
-	}
-	else
-	{
-	printf("is negetive.\n");
-	}
-		return 0;
+int n;
+srand(time(NULL));
+n = rand();
+printf("The number %d ", n);
+if (n > 0)
+{
+printf("is positive.\n");
 }
+else if (n == 0)
+{
+printf("is zero.\n");
+}
+else
+{
+printf("is negative.\n");
+}
+return (0);
+}
+
