0dbe913e6efd584f438cd97602ff4473ae81d64f
eli8665 <elham2707@gmail.com>
2024-06-19 13:09:40 +0000

    project 19 task 1-0

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ * n is positive, zero, or negative.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int y;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+y = n % 10;
+if (y > 5)
+{
+printf("greater than 5\n");
+}
+else if (y == 0)
+{
+printf("0\n");
+}
+else
+{
+printf("less than 6 and not 0\n");
+}
+return (0);
+}
