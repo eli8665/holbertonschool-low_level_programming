9b03047fd0a94bf460db64b9b939bac1922bfb16
eli8665 <elham2707@gmail.com>
2024-06-20 19:46:31 +0000

    project 9 task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print singel digit 0 -10,
+*NEW LINE.
+*Return: always 0 (success)
+*/ 
+int main(void)
+{
+int num;
+for (num = 0; num < 10 ; num ++)
+{
+putchar(num + '0');
+}
+
+putchar('\n');
+
+return (0);
+}
